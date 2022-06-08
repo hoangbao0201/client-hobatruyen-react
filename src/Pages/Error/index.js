b5ef33a90b592872f54475548cfab9cd329df9a4
hoangbao020103 <hoangbao020103@gmail.com>
2022-06-08 09:38:00 +0700

    more

--- src/Pages/Error/index.js
+++ src/Pages/Error/index.js
@@ -1,10 +1,24 @@
 import classNames from 'classnames/bind';
+import { Link } from 'react-router-dom';
 import styles from './Error.module.scss';
 
 const cx = classNames.bind(styles);
 
 function Error() {
-    return <div className={cx('wrapper')}>404</div>;
+    return (
+        <div className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                <h1 className={cx('container')}>
+                    <span className={cx('text')}>404</span>
+                    <div className={cx('title')}>
+                        <Link className={cx('link')} to="/">
+                            Quay lại trang chủ
+                        </Link>
+                    </div>
+                </h1>
+            </div>
+        </div>
+    );
 }
 
 export default Error;
