38fda095c8faed33902490daabc125582d1d6ffc
hoangbao020103 <hoangbao020103@gmail.com>
2022-06-08 08:56:59 +0700

    navdown modal

--- src/Layout/library/Button/index.js
+++ src/Layout/library/Button/index.js
@@ -3,8 +3,8 @@ import styles from './Button.module.scss';
 
 const cx = classNames.bind(styles);
 
-function Button({ children }) {
-    return <div className={cx('wrapper')}>{children}</div>;
+function Button({ children, className }) {
+    return <div className={cx('wrapper', className)}>{children}</div>;
 }
 
 export default Button;
